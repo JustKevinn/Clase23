eb178906ec697af1db981dd6b7ff0cafd4824782
JustKevinn <kevinrodriguez7411@gmail.com>
2022-06-09 12:21:50 -0300

    subiendo archivos

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ let express = require('express')
 let path = require('path')
 let app = express()
 let port = 3000
-let dirname = path.join(__dirname,)
+
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req,res) => res.send('Bienvenidos a nuestra pagina'))
 app.get('/home',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
 
-app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:3000/${port}\n${dirname}`))
+app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}\n${dirname}`))
