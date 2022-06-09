1a39c197f35968abc35e45f240a2cfe206d7f6c7
JustKevinn <kevinrodriguez7411@gmail.com>
2022-06-09 12:26:43 -0300

    modifico archivo .png

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.use(express.static(path.join(__dirname,'public')))
 app.get('/', (req,res) => res.send('Bienvenidos a nuestra pagina'))
 app.get('/home',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
 
-app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}\n${dirname}`))
+app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}\n${__dirname}`))
