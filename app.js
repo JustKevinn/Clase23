e0750ec667b4e643743032899dc9a5d4b54f48be
JustKevinn <kevinrodriguez7411@gmail.com>
2022-06-09 23:50:21 -0300

    Agrego rutas para navegar más fácilmente

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req,res) => res.send('Bienvenidos a nuestra pagina'))
-app.get('/home',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+/*app.get('/', (req,res) => res.send('Bienvenidos a nuestra pagina'))*/
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
+app.get('/register',(req,res) =>res.sendFile(path.resolve(__dirname,'views','register.html')))
 
 app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}\n${__dirname}`))
